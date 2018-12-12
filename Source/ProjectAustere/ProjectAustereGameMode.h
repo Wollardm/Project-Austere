37fed9a9335ce58f56cb5ea2c2ce14da4d9fe562
Wollardm <michaelawollard@gmail.com>
2018-12-11 22:40:20 -0500

    Added unreal project.

---
+++ Source/ProjectAustere/ProjectAustereGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectAustereGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectAustereGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectAustereGameMode();
+};
+
+
+
